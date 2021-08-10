d0a3efa0840027d98f59f50e2f22f5269fd9a89b
dupilop <abhinav.kafle@gmail.com>
2021-08-10 17:14:20 +0545

    newprofileupdate

--- backend/pages/editprofile.php
+++ backend/pages/editprofile.php
@@ -4,19 +4,19 @@ $abc = new DatabaseTable('admins');
 if (isset($_POST['update'])) {
 
 
-	if (!$_FILES['a_new_profile_image']['name'] == '') {
-		$_POST['a_profile_image'] = $_FILES['a_new_profile_image']['name'];
-	}
-	if ($_FILES['a_new_profile_image']['error'] == 0) {
-		$fileName = $_FILES['a_new_profile_image']['name'];
-		move_uploaded_file($_FILES['a_new_profile_image']['tmp_name'], '../images/profile/' . $fileName);
-	}
-	unset($_POST['update'], $_POST['a_new_profile_image']);
-	$ins = $abc->update($_POST, 'a_id');
-	header('location: profile');
+    if (!$_FILES['a_new_profile_image']['name'] == '') {
+        $_POST['a_profile_image'] = $_FILES['a_new_profile_image']['name'];
+    }
+    if ($_FILES['a_new_profile_image']['error'] == 0) {
+        $fileName = $_FILES['a_new_profile_image']['name'];
+        move_uploaded_file($_FILES['a_new_profile_image']['tmp_name'], './api/images/profile/' . $fileName);
+    }
+    unset($_POST['update'], $_POST['a_new_profile_image']);
+    $ins = $abc->update($_POST, 'a_id');
+    header('location: profile');
 }
 if (!isset($_SESSION['id'])) {
-	header('Location: ../login.php');
+    header('Location: ../login.php');
 }
 $abc = $pdo->query("SELECT * FROM admins WHERE a_id=" . $_SESSION['id'])->fetch();
 
